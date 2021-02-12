f9ccde60af06d99e4be44a2f3ef47a1a3ad51448
lxp138 <76868309+lxp138@users.noreply.github.com>
2021-02-12 23:19:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,7 +36,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-s
 #强制关机插件
 #git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 #自动关机插件
-git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
+#git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
 
 # argon主题
 pushd package/lean
